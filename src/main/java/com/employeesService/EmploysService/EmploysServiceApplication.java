cafcc4027d4813856dcb61ad6dd735dd423e0a13
Taulantfazliu <talifazliu@gmail.com>
2021-10-11 14:18:32 +0200

    initial commit

---
+++ src/main/java/com/employeesService/EmploysService/EmploysServiceApplication.java
@@ -0,0 +1,13 @@
+package com.employeesService.EmploysService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploysServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploysServiceApplication.class, args);
+	}
+
+}
